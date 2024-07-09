e8f31950d325ecaa2f2863cdbc4b529e895c6924
wdh970616 <wdh970616@naver.com>
2024-07-09 15:42:40 +0900

    handler method

---
+++ chap02-handler-method-lecture-source/src/test/java/com/seongmin/handlermethod/Chap02HandlerMethodLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seongmin.handlermethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap02HandlerMethodLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
