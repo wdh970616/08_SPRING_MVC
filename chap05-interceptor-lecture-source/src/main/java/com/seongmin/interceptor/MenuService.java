b37aa8b25f3d6fecdbd1e64a596a46a061b1d186
wdh970616 <wdh970616@naver.com>
2024-07-10 13:09:39 +0900

    interceptor

---
+++ chap05-interceptor-lecture-source/src/main/java/com/seongmin/interceptor/MenuService.java
@@ -0,0 +1,11 @@
+package com.seongmin.interceptor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuService {
+
+    public void method() {
+        System.out.println("MenuService 호출됨...");
+    }
+}
