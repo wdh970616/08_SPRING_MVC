bbc02fc4bd3a88d6846ce9846e4f88794cc20f87
wdh970616 <wdh970616@naver.com>
2024-07-31 15:00:45 +0900

    타임리프

---
+++ chap07-thymeleaf-lecture-source/src/main/java/com/seongmin/thymeleaf/model/dto/MemberDTO.java
@@ -0,0 +1,16 @@
+package com.seongmin.thymeleaf.model.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+
+    private String name;
+    private int age;
+    private char gender;
+    private String address;
+}
