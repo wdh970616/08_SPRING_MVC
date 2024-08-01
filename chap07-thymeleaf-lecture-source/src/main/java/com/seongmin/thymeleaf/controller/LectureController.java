f50970bd28e5f2e1f00d72c9e3e45d7b75c6118d
wdh970616 <wdh970616@naver.com>
2024-08-01 13:19:31 +0900

    타임리프

--- chap07-thymeleaf-lecture-source/src/main/java/com/seongmin/thymeleaf/controller/LectureController.java
+++ chap07-thymeleaf-lecture-source/src/main/java/com/seongmin/thymeleaf/controller/LectureController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("lecture")
@@ -16,6 +18,7 @@ public class LectureController {
 
     @GetMapping("expression")
     public ModelAndView expression(ModelAndView mv) {
+
         mv.addObject("member", new MemberDTO("홍길동", 20, '남', "서울시 서초구"));
         mv.addObject("hello", "hello!<h3>Thymeleaf</h3>");
 
@@ -25,6 +28,7 @@ public class LectureController {
 
     @GetMapping("conditional")
     public ModelAndView conditional(ModelAndView mv) {
+
         mv.addObject("num", 1);
         mv.addObject("str", "바나나");
 
@@ -41,11 +45,48 @@ public class LectureController {
 
     @GetMapping("etc")
     public ModelAndView etc(ModelAndView mv) {
-        SelectCriteria selectCriteria = new SelectCriteria(1, 10, 5);
 
+        SelectCriteria selectCriteria = new SelectCriteria(1, 10, 5);
         mv.addObject(selectCriteria);
+
+        MemberDTO member = new MemberDTO("홍길동", 20, '남', "서울시 서초구");
+        mv.addObject("member", member);
+
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO("홍길동", 20, '남', "서울시 서초구"));
+        memberList.add(new MemberDTO("유관순", 22, '여', "서울시 노원구"));
+        memberList.add(new MemberDTO("장보고", 40, '남', "서울시 종로구"));
+        memberList.add(new MemberDTO("신사임당", 30, '여', "서울시 성북구"));
+        mv.addObject("memberList", memberList);
+
+        Map<String, MemberDTO> memberMap = new HashMap<>();
+        memberMap.put("m01", new MemberDTO("홍길동", 20, '남', "서울시 서초구"));
+        memberMap.put("m02", new MemberDTO("유관순", 22, '여', "서울시 노원구"));
+        memberMap.put("m03", new MemberDTO("장보고", 40, '남', "서울시 종로구"));
+        memberMap.put("m04", new MemberDTO("신사임당", 30, '여', "서울시 성북구"));
+        mv.addObject("memberMap", memberMap);
 
         mv.setViewName("lecture/etc");
         return mv;
+    }
+
+    @GetMapping("fragment")
+    public ModelAndView fragment(ModelAndView mv) {
+
+        mv.addObject("test1", "value1");
+        mv.addObject("test2", "value2");
+
+        mv.setViewName("lecture/fragment");
+        return mv;
+    }
+
+    @GetMapping("page")
+    public String page() {
+        return "page";
+    }
+
+    @GetMapping("page2")
+    public String page2() {
+        return "page2";
     }
 }
